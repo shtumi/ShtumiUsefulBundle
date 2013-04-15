c92fdee3993c986364ef3835b5d408fbf57c8012
jcrapet <jcrapet@altima.fr>
2013-04-15 15:18:50 +0200

    Multiple fields in Ajax AutoComplete render

--- Controller/AjaxAutocompleteJSONController.php
+++ Controller/AjaxAutocompleteJSONController.php
@@ -15,7 +15,6 @@ class AjaxAutocompleteJSONController extends Controller
 
     public function getJSONAction()
     {
-
         $em = $this->get('doctrine')->getEntityManager();
         $request = $this->getRequest();
 
@@ -34,45 +33,69 @@ class AjaxAutocompleteJSONController extends Controller
         switch ($entity_inf['search']){
             case "begins_with":
                 $like = $letters . '%';
-            break;
+                break;
             case "ends_with":
                 $like = '%' . $letters;
-            break;
+                break;
             case "contains":
                 $like = '%' . $letters . '%';
-            break;
+                break;
             default:
                 throw new \Exception('Unexpected value of parameter "search"');
         }
 
-	$property = $entity_inf['property'];
+        $property = $entity_inf['property'];
 
         if ($entity_inf['case_insensitive']) {
-                $where_clause_lhs = 'WHERE LOWER(e.' . $property . ')';
-                $where_clause_rhs = 'LIKE LOWER(:like)';
+            $where_clause_lhs = 'WHERE LOWER(e.' . $property . ')';
+            $where_clause_rhs = 'LIKE LOWER(:like)';
         } else {
 
-                $where_clause_lhs = 'WHERE e.' . $property;
-                $where_clause_rhs = 'LIKE :like';
+            $where_clause_lhs = 'WHERE e.' . $property;
+            $where_clause_rhs = 'LIKE :like';
         }
 
+        $properties = $entity_inf['shown_properties'];
+        $last_key = count($properties) - 1;
+        $toSelect = null;
 
+        foreach($properties as $key => $prop)
+        {
+            $toSelect .= 'e.'.$prop;
+            if ($key != $last_key) $toSelect .= ', ';
+        }
 
         $results = $em->createQuery(
-            'SELECT e.' . $property . '
+            'SELECT '.$toSelect.'
              FROM ' . $entity_inf['class'] . ' e ' .
-             $where_clause_lhs . ' ' . $where_clause_rhs . ' ' .
-            'ORDER BY e.' . $property)
+                $where_clause_lhs . ' ' . $where_clause_rhs . ' ' .
+                'ORDER BY e.' . $property)
             ->setParameter('like', $like )
             ->setMaxResults($maxRows)
             ->getScalarResult();
 
-        $res = array();
-        foreach ($results AS $r){
-            $res[] = $r[$entity_inf['property']];
+        $final = array();
+
+        foreach ($results as $key => $res)
+        {
+            $stringToPrint = null;
+            $i = 0;
+            $last_key = count($res) - 1;
+
+            foreach($res as $r)
+            {
+                $stringToPrint .= $r;
+
+                if ($i !== $last_key)
+                {
+                    $stringToPrint .= ' - ';
+                }
+                $i++;
+            }
+
+            $final[$key] = $stringToPrint;
         }
 
-        return new Response(json_encode($res));
-
+        return new Response(json_encode($final));
     }
 }
